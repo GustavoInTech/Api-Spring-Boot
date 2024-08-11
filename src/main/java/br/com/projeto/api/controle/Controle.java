19d843ed7081ccd1a1fb4ccce5ec4f5db721cfdd
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-11 15:53:56 -0300

    enviando via requestBody

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -2,7 +2,11 @@ package br.com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.modelo.Pessoa;
 
 @RestController
 public class Controle {
@@ -20,5 +24,11 @@ public class Controle {
     @GetMapping("/boasVindas/{nome}")
     public String boasVindas(@PathVariable String nome) {
         return "Seja bem Vindo(a) " + nome;
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+
+        return p;
     }
 }
--- src/main/java/br/com/projeto/api/modelo/Pessoa.java
+++ src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -1,5 +1,25 @@
 package br.com.projeto.api.modelo;
 
 public class Pessoa {
+    // atributos
+    private String nome;
+    private int idade;
+
+    // Get e Set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 
 }
